5b8d5d1c8aafd96b14a2b360eed0ccc2f4f58ce4
Lucy Gelderloos <lucygel@gmail.com>
2022-06-10 21:38:26 -0700

    Create multiplyArray function

--- js/app.js
+++ js/app.js
@@ -93,11 +93,11 @@ function sumArray(sumArr) { //eslint-disable-line
   return [addedArray,a + ',' + b + ',' + c + ' was passed in as an array of numbers, and ' + addedArray + ' is their sum.'];
 }
 
-sumArray(testArray);
+//sumArray(testArray);
 
 // Here is the test for sumArray(); uncomment it to run it
 
-testSumArray(testArray);
+//testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -113,11 +113,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+  let a = multArr[0];
+  let b = multArr[1];
+  let c = multArr[2];
+  let multArray = multiply(a,b,c);
+  return [multArray,'The numbers ' + a + ',' + b + ',' + c + ' have a product of ' + multArray + '.'];
 }
 
+// multiplyArray(testArray);
+
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+ testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
